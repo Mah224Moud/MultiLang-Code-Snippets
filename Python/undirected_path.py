1fd723779fa41c3581ce1f8d38c0d0a67ca252b4
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2024-08-22 08:56:14 +0200

    Finished the undirected path using bfs

--- Python/undirected_path.py
+++ Python/undirected_path.py
@@ -1,45 +1,70 @@
 def undirected_path(edges, node_A, node_B):
+    """
+    This function checks if there is a path between two nodes in an undirected graph.
+
+    Args:
+        edges (list): A list of tuples representing the edges in the graph.
+        node_A (str): The node to start the search from.
+        node_B (str): The node to search for.
+
+    Returns:
+        bool: True if there is a path from node_A to node_B, False otherwise.
+    """
     graph = generate_graph(edges)
-    print(graph)
     visited = set()
     queue = [node_A]
-    while(len(queue) > 0):
+    while (len(queue) > 0):
         current = queue[0]
         queue.pop(0)
-        print(f"Current: {current}")
-        if current == node_B: return True
+        if current == node_B:
+            return True
         if current not in visited:
             visited.add(current)
-            print(f"Visited {visited}")
             for i in graph[current]:
-               print(i)
-            """visited.add(current)
-            for neigbhor in graph[current]:
-                print(f"Neigbhor {neigbhor}")
-                queue.append(neigbhor)
-                print(f"Visited {visited}")
-                print(f"Current queue: {queue}")"""
-    return False  
+                queue.append(i)
+    return False
+
 
 def generate_graph(edges: list) -> dict:
-  g = {}
-  for (a, b) in edges:
-    if a in g:
-      g[a].append(b)
-    else:
-      g[a] = [b]
-    if b in g:
-      g[b].append(a)
-    else:
-      g[b]= [a]
-  return g
+    """
+    This function generates the adjacency list from a list of edges reprensenting an undirected graph.
 
-edges = [
-  ('i', 'j'),
-  ('k', 'i'),
-  ('m', 'k'),
-  ('k', 'l'),
-  ('o', 'n')
-]
+    Args:
+        edges (list): A list of tuples representing the edges in the graph.
 
-print(undirected_path(edges, 'j', 'm')) # -> True
+    Returns:
+        dict: A dictionary representing the adjacency list of the graph.
+    """
+    graph = {}
+    for (a, b) in edges:
+        if a in graph:
+            graph[a].append(b)
+        else:
+            graph[a] = [b]
+        if b in graph:
+            graph[b].append(a)
+        else:
+            graph[b] = [a]
+    return graph
+
+
+def main():
+
+    edges = [
+        ('i', 'j'),
+        ('k', 'i'),
+        ('m', 'k'),
+        ('k', 'l'),
+        ('o', 'n')
+    ]
+    src = "j"
+    dst = 'm'
+    has_path = undirected_path(edges, src, dst)
+    if has_path:
+        print(f"{edges} from {src} to {dst} has path")
+    else:
+        print(f"{edges} from {src} to {dst} has not path")
+
+
+if __name__ == "__main__":
+    main()
